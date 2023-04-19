6b669cf07e481f3a8c13061eb8526ac200a3069d
GnagetEmil <emil@gnaget.se>
2023-04-19 16:18:01 +0200

    added app.get with res.sendFile

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-// setup mongoose connection
-const mongoose = require('mongoose');
-mongoose.set("strictQuery", false);
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// DB url
-const mongoDB = ""
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.CONNECTION_URL;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-main().catch((err) => console.log(err));
-async function main() {
-    await mongoose.connect(mongoDB);
+
+// Get index.html
+// sendFile will go here
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start server
+app.listen(process.env.PORT, () => console.log('Example app is listening on port 3000.'));
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
+run().catch(console.dir);
+
+
