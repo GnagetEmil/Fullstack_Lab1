5109803e020f9209947e7460a6d76cda964514f8
GnagetEmil <emil@gnaget.se>
2023-04-20 13:14:53 +0200

    added route handling for all albums and specific

--- index.js
+++ index.js
@@ -24,19 +24,68 @@ app.get('/', function (req, res) {
 // Start server
 app.listen(process.env.PORT, () => console.log('Example app is listening on port 3000.'));
 
+app.get('/api/albums', async (req, res) => {
+    console.log(await getAllAlbums())
+    const albumsData = await getAllAlbums();
+    res.send(albumsData);
+})
 
-async function run() {
+app.get('/api/albums/:title', async (req, res) => {
+    const title = req.params.title;
+    const albumData = await getAlbumByTitle(title);
+    if (albumData) {
+        res.send(albumData);
+    } else {
+        res.status(404).send("Album not found");
+    }
+});
+
+
+async function getAllAlbums() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const cursor = client.db("MusicAlbums").collection("Albums").find();
+        const results = await cursor.toArray();
+
+        return (JSON.stringify(results));
+    } catch (error) {
+        console.error(error);
     } finally {
-        // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
-run().catch(console.dir);
+
+async function getAlbumByTitle(title) {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const query = { title: title };
+        const albumData = await client.db("MusicAlbums").collection("Albums").find(query).toArray();
+
+        if (albumData.length === 0) {
+            return null;
+        } else if (albumData.length === 1) {
+            return albumData[0];
+        } else {
+            return albumData;
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+}
+
+
+
+
+
+
+
 
 
