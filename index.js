23f42aa506b2b0022dbce350ec556fd5eb677b70
GnagetEmil <emil@gnaget.se>
2023-04-20 16:14:21 +0200

    add to database. show albums

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.CONNECTION_URL;
@@ -18,7 +22,7 @@ const client = new MongoClient(uri, {
 // Get index.html
 // sendFile will go here
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // Start server
@@ -37,6 +41,24 @@ app.get('/api/albums/:title', async (req, res) => {
         res.send(albumData);
     } else {
         res.status(404).send("Album not found");
+    }
+});
+
+app.post('/api/albums', async (req, res) => {
+    const { title, artist, year } = req.body;
+    console.log(`Title: ${title}, Artist: ${artist}, Year: ${year}`);
+
+
+    console.log(await isAlbumInDatabase(title, artist, year))
+    // Check if album exists in db
+    if (await isAlbumInDatabase(title, artist, year)) {
+        const message = 'Status: 409 Album already exists';
+        return res.status(409).json({ message });
+    }
+    else {
+        addAlbumToDatabase(title, artist, year)
+        const message = 'Status: 201 Album has been added to database';
+        return res.status(201).json({ message });
     }
 });
 
@@ -81,11 +103,43 @@ async function getAlbumByTitle(title) {
     }
 }
 
+async function isAlbumInDatabase(title, artist, year) {
+    try {
+        await client.connect();
+        const query = {
+            title: title,
+            artist_name: artist,
+            year: year
+        };
+        const albumData = await client.db("MusicAlbums").collection("Albums").find(query).toArray();
+        if (albumData.length == 0) {
+            return false;
+        }
+        else if (albumData.length >= 1) {
+            return true;
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 
+async function addAlbumToDatabase(title, artist, year) {
+    try {
+        const database = client.db("MusicAlbums");
+        const albums = database.collection("Albums");
+        const doc = {
+            title: title,
+            artist_name: artist,
+            year: year
+        }
+        const result = await albums.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
 
+    } finally {
 
+        await client.close();
 
-
-
-
+    }
+}
 
